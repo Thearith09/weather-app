dc09f5b3f04d11ddf8f19fdcc4e2d2a00200727f
Thearith09 <puthearith0909@gmail.com>
2020-10-04 16:23:30 +0700

    change_name_PORT

--- src/index.js
+++ src/index.js
@@ -77,5 +77,5 @@ app.get('*', (req, res) => {
 });
 
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is up on port ${port}.`));
